51d0fbd755bb376cccc19e135d7885e3ccce65d4
h3vanth <rhevanthraj@gmail.com>
2024-06-02 14:21:25 +0530

    test: Add basic product service tests

---
+++ src/test/java/io/bbw/dmc/service/ProductServiceIT.java
@@ -0,0 +1,71 @@
+package io.bbw.dmc.service;
+
+import io.bbw.dmc.exception.EntityNotFoundException;
+import io.bbw.dmc.model.Product;
+import io.bbw.dmc.model.User;
+import io.bbw.dmc.util.JwtUtils;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Query;
+import org.springframework.http.HttpHeaders;
+
+import java.math.BigDecimal;
+
+@SpringBootTest
+public class ProductServiceIT {
+    private static HttpHeaders httpHeaders;
+
+    private static final Product PRODUCT_1 = new Product("123", "1", "Biryani", new BigDecimal(150), true, "", 0, "", null);
+
+    @Autowired
+    private MongoTemplate mongoTemplate;
+
+    @Autowired
+    private ProductService productService;
+
+    @BeforeAll
+    static void setupAuthToken() {
+        JwtUtils.setSecret("8a48UWvG#s:@2");
+        var httpHeaders = new HttpHeaders();
+        httpHeaders.setBearerAuth(JwtUtils.generateToken(new User("1", "hr@gmail.com", "12345678", "123456", null)));
+        ProductServiceIT.httpHeaders = httpHeaders;
+    }
+
+    @AfterEach
+    public void afterEach() {
+        mongoTemplate.remove(new Query(), Product.class);
+    }
+
+    @Test
+    void addProductTest() {
+        productService.addProduct(PRODUCT_1, null, "1");
+
+        Assertions.assertNotNull(mongoTemplate.findById("123", Product.class));
+    }
+
+    @Test
+    void getProductTest() {
+        productService.addProduct(PRODUCT_1, null, "1");
+
+        Assertions.assertNotNull(productService.getProduct("123"));
+    }
+
+    @Test
+    void getProductsTest() {
+        productService.addProduct(PRODUCT_1, null, "1");
+
+        Assertions.assertEquals(1, productService.getProducts("1").size());
+    }
+
+    @Test
+    void getProduct_InvalidProductIdTest() {
+        Assertions.assertThrows(EntityNotFoundException.class, () -> {
+            productService.getProduct("234");
+        });
+    }
+}
